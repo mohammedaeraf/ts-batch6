116373001aa499fb0c2f5d11e45bd47d3e56da3a
Mohammed Aeraf <aeraf@outlook.com>
2026-04-21 21:04:15 +0530

    minor comment

--- typeinference.ts
+++ typeinference.ts
@@ -1,4 +1,4 @@
-// Type Inference
+// Type Inference is a powerful feature in TypeScript that allows the compiler to automatically determine the type of a variable based on its initial value. This can help reduce the amount of code you need to write and make your code more concise. However, it can also lead to errors if you try to assign a value of a different type to a variable that has already been inferred.
 let n = 200;
 n = "two hundred";
 
